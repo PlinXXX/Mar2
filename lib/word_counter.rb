485384f188f7ad7977ee09dc87973bbd728bc565
santatramaximin <santatramaximin@gmail.com>
2019-01-16 08:50:56 +0100

    Finished counter

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -3,7 +3,6 @@ dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 ###################DECOMPOSEUR DE MOT####################
 	def decompose(word)
 		word_decompose = []
-		w = ''
 		for j in 0...word.length
 			w = ''
 			for i in j...word.length
@@ -15,34 +14,19 @@ dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 	end
 #########################################################
 
-
-def number_of_word(word, dictionary)
-	number_of_word = 0
-	dictionary.each { |w| number_of_word += 1 if w == word }
-	return number_of_word
-end
-
-
 def word_counter(word, dictionary)
 
-	char_in_word = []
-	times = []
 	word_decompose = decompose(word)
-		word_decompose.each do |word|  
-			if dictionary.include?(word)
-				char_in_word << word
-
-				times << number_of_word(word, dictionary)
-			end
-		end
-	puts char_in_word.inspect
-	puts times.inspect 
-	recap = Hash[char_in_word.zip(times)]
-	recap.each do
-		#NOT FINISHED
-		word_and_repetition
+	word_decompose.map!(&:downcase)
+	char_in_word = word_decompose.keep_if {|mot| dictionary.include?(mot)}
+	char_in_word = char_in_word.map!(&:downcase)
+	void = Array.new(char_in_word.length, 0)
+	h = Hash[char_in_word.zip(void)]
+	
+	char_in_word.each do |key|
+		h[key] += 1
 	end
-	puts word_and_repetition.inspect
-end
 
-word_counter("Howdy partner, sit down! How's it going?", dictionary)
+	return h
+end
+puts word_counter("Howdy partner, sit down! How's it going?", dictionary)
