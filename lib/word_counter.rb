41aeaab7d22d19464b25560e27eb015c9d5166d5
santatramaximin <santatramaximin@gmail.com>
2019-01-16 09:29:51 +0100

    The end?

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,4 +1,4 @@
-dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+@dictionnary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
 ###################DECOMPOSEUR DE MOT####################
 	def decompose(word)
@@ -14,19 +14,23 @@ dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 	end
 #########################################################
 
-def word_counter(word, dictionary)
+def word_counter(word, dictionnary)
 
 	word_decompose = decompose(word)
 	word_decompose.map!(&:downcase)
-	char_in_word = word_decompose.keep_if {|mot| dictionary.include?(mot)}
-	char_in_word = char_in_word.map!(&:downcase)
+	word_decompose.keep_if {|mot| @dictionnary.include?(mot)}
+
+	#create the hash where you put the words
+	char_in_word = word_decompose
 	void = Array.new(char_in_word.length, 0)
 	h = Hash[char_in_word.zip(void)]
 	
+	#count the words
 	char_in_word.each do |key|
 		h[key] += 1
 	end
 
 	return h
 end
-puts word_counter("Howdy partner, sit down! How's it going?", dictionary)
+
+puts word_counter("Howdy partner, sit down! How's it going?", @dictionnary)
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/word_counter'
 
 describe "what word_counter" do
-	it "" do
-		expect(word_counter("below", dictionary)).to eq({"below"=>1, "low"=>1})
-		expect(word_counter("Howdy partner, sit down! How's it going?", dictionary)).to eq({"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "sit"=>1, "i"=>3, "it"=>2, "down"=>1, "own"=>1, "go"=>1, "going"=>1})
+	it "should return the frequency of word in 'word' in '@dictionnary'" do
+		expect(word_counter("below" , @dictionnary)).to eq({"below"=>1, "low"=>1})
+		expect(word_counter("Howdy partner, sit down! How's it going?" , @dictionnary)).to eq({"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "sit"=>1, "i"=>3, "it"=>2, "down"=>1, "own"=>1, "go"=>1, "going"=>1})
 	end
 end
